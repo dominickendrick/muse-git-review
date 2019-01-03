afabb835be6d896bbd59fb37e135f304bab89f74
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2019-01-03 16:51:23 +0000

    adding chrome extension

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
-import _ from 'lodash';
+import { MuseClient } from "muse-js";
+import { EEGReading } from "muse-js/dist/lib/muse-interfaces.d.ts";
 
-function component() {
-  let element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let client: MuseClient = new MuseClient();
 
-  return element;
+function connectButton() {
+  let button = document.createElement("button");
+  button.textContent = "Connect";
+  button.addEventListener("click", connect);
+
+  return button;
 }
 
-document.body.appendChild(component());
+function pauseButton() {
+  let button = document.createElement("button");
+  button.textContent = "Pause";
+  button.addEventListener("click", pause);
+
+  return button;
+}
+
+
+async function connect () {
+  await client.connect();
+  await client.start();
+  stream();
+}
+
+async function pause () {
+  await client.pause();
+}
+
+function stream () {
+  client.eegReadings
+    .subscribe( (eeg: EEGReading) => { console.log(eeg) });
+}
+
+document.body.appendChild(connectButton());
+document.body.appendChild(pauseButton());
